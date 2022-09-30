628f1418a7b432618f6f1c727a976fba6dc9888f
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-09-30 01:40:19 -0300

    nav corrections

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -14,15 +14,15 @@ const Footer = () => {
         <footer>
         <div className="bottom_end">
             <div className="bottom_end_data">
-                <p>Abel Martin Bergesio - 2022 - Buenos Aires, Argentina.</p>
+                <p>Abel Martín Bergesio - 2022 - Buenos Aires, Argentina.</p>
             </div>
             <nav>
-                <Item img={about} title="sobre mi" route="#aboutme" />
+                <Item img={about} title="sobre mí" route="#aboutme" />
                 <Item img={techs} title="technologías" route="#technologies" />
                 <Item img={portfolio} title="portfolio" route="#portfolio" />
                 <Item img={github} title="github" route="https://github.com/ambergesio" target="_blank"/>
-                <Item img={linkedin} title="Linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
-                <Item img={contact} title="Contacto" route="#contact" />
+                <Item img={linkedin} title="linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
+                <Item img={contact} title="contacto" route="#contact" />
             </nav>
         </div>
     </footer>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -19,12 +19,12 @@ const Header = () => {
 
                 <div className="nav_container">
                     <nav>
-                        <Item img={about} title="sobre mi" route="#aboutme" />
+                        <Item img={about} title="sobre mí" route="#aboutme" />
                         <Item img={techs} title="tecnologías" route="#technologies" />
                         <Item img={portfolio} title="portfolio" route="#portfolio" />
                         <Item img={github} title="github" route="https://github.com/ambergesio" target="_blank"/>
-                        <Item img={linkedin} title="Linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
-                        <Item img={contact} title="Contacto" route="#contact" />
+                        <Item img={linkedin} title="linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
+                        <Item img={contact} title="contacto" route="#contact" />
                     </nav>
                 </div>
 
@@ -32,12 +32,12 @@ const Header = () => {
                     <div className="menu_hamb"></div>
                 </div>
                 <div className={!open ? "mobile_nav closed" : "mobile_nav open"} onClick={() => setOpen(false)}>
-                    <Item img={about} title="sobre mi" route="#aboutme" />
+                    <Item img={about} title="sobre mí" route="#aboutme" />
                     <Item img={techs} title="tecnologías" route="#technologies" />
                     <Item img={portfolio} title="portfolio" route="#portfolio" />
                     <Item img={github} title="github" route="https://github.com/ambergesio" target="_blank"/>
-                    <Item img={linkedin} title="Linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
-                    <Item img={contact} title="Contacto" route="#contact" />
+                    <Item img={linkedin} title="linkedin" route="https://www.linkedin.com/in/abel-martin-bergesio-42819820a/" target="_blank"/>
+                    <Item img={contact} title="contacto" route="#contact" />
                 </div>
 
 
